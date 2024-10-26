7042db40e9531e160287f707b3a090358b1b66f8
Chris528R <crodriguezm2001@alumno.ipn.mx>
2024-10-25 18:47:25 -0600

    Ya tengo la forma de leer los bytes
    Ya se guarda dentro del arreglo y su frecuencia

--- ArbolHuffman.h
+++ ArbolHuffman.h
@@ -18,6 +18,7 @@ typedef struct elemento
 {
 	int frecuencia;
 	byte caracter;
+	void *apellido;
 } elemento;
 
 typedef struct nodo
--- prueba.c
+++ prueba.c
@@ -3,21 +3,83 @@
 #include "ArbolHuffman.h"
 #include "Heap.h"
 
+elemento* cargarElementos(char *nombreArchivo, int *tamanoArreglo){
+	int j;
+	size_t tamanioArchivo, i;
+	byte byteLeido;
+	elemento *A;
+	booleano cambios;
+	
+	FILE *archivo = fopen(nombreArchivo, "rb"); //Abrimos el archivo en modo binario
+	if (archivo == NULL) {
+        printf("Error al abrir el archivo");
+        exit(1);
+    }
+	
+	// Mueve el puntero del archivo al final para obtener el tamaño
+    fseek(archivo, 0, SEEK_END);
+    tamanioArchivo = ftell(archivo); // Obtiene el tamaño del archivo en bytes
+    rewind(archivo); // Regresa el puntero al inicio
+
+	//En nuestra estructura elemento, ya tenemos el unsigned char como atributo, entonces
+    A = (elemento*)malloc(256 * sizeof(elemento));
+	//Arreglo de elementos, en estos vamos a guardar los bytes que ya conocemos y su frecuencia
+    if (A == NULL) {
+        printf("Error al asignar memoria para el arreglo de elementos");
+        fclose(archivo);
+        exit(1);
+    }
+	
+	*tamanoArreglo = 0; //Guardamos cuantos bytes llevamos guardados, ya que puede suceder que se repitan o no
+	// Lee cada byte del archivo y guárdalo en el campo `caracter` de cada estructura
+    for (i = 0; i < tamanioArchivo; i++) {
+        fread(&byteLeido, sizeof(byte), 1, archivo);
+		//Hay que verificar si este byte, ya se encuentra dentro del arreglo, si es así, solo aumentamos su frecuencia
+		//Se puede optimizar usando un mapa o multiset, sin embargo, que flojera implementarlo en C, entonces hacemos 
+		//una búsqueda lineal, viendo si se encuentra el elemento
+		if(*tamanoArreglo == 0){
+			A[0].caracter = byteLeido;
+			A[0].frecuencia = 1;
+			(*tamanoArreglo)++;
+		}else{
+			cambios = FALSE;
+			for(j = 0; j < *tamanoArreglo; j++){
+				if(A[j].caracter == byteLeido){
+					A[j].frecuencia++;
+					cambios = TRUE;
+					break;
+				}
+			}
+			if(cambios == FALSE){
+				A[*tamanoArreglo].caracter = byteLeido;
+				A[*tamanoArreglo].frecuencia = 1;
+				(*tamanoArreglo)++;
+			}
+		}
+    }
+	fclose(archivo);
+	return A;
+}
 
 int main() {
-	int n = 6;
-	int frecuencias[6] = { 45, 13, 12, 16, 9, 5 };
-	int i;
-	elemento e[n];
-	for(i = 0; i < n; i++){
-		e[i].frecuencia = frecuencias[i];
-	}
+	//LECTURA DE ARCHIVOS POR BITS
+	int tamanoArreglo, i;
+	char nombreArchivo[20];
 	
+	printf("Ingresa el nombre del archivo a leer en bits: \n");
+	scanf("%s", nombreArchivo);
+	
+	elemento *arreglo = cargarElementos(nombreArchivo, &tamanoArreglo);
+	
+	for(i = 0; i < tamanoArreglo; i++){
+		printf("Elemento %d: caracter = %02X\n Y frecuencia: %d\n", i, arreglo[i].caracter, arreglo[i].frecuencia);
+	}
+	/*
 	arbolHuffman A;
 	
-	A = CrearArbol(e, n);
+	A = CrearArbol(arreglo, tamanoArreglo);
 	printf("frecuencia retornada: %d\n", A->e.frecuencia);
-	
+	*/
 	
     return 0;
 }
