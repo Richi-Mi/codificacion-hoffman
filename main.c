cb7af1f22caaf1dde6ce976a8c59446c43cf29ec
Jose Ricardo Mendoza Castañeda <joseanakin@outlook.es>
2024-11-02 13:40:20 -0600

    Empezando de 0 pero recios

--- main.c
+++ main.c
@@ -1,21 +1,54 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include "ArbolHuffman.h"
-#include "Heap.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "arbol.c"
+
+void addTreeLef( NodoArbol **p, int f, char c ){
+
+    NodoArbol *new = (NodoArbol *) malloc(sizeof(NodoArbol));
+
+    new -> frecuencia = f;
+    new -> caracter = c;
+    new -> sig = NULL;
+
+    if (*p == NULL){
+        *p = new;
+    }
+
+    else{
+        
+        // aux -> Accede al valor al que apunta 'p' (dirección de estructura)
+        NodoArbol *aux = *p;
+
+        // Pregunta sobre el nodo siguiente al que esta apunta
+        while (aux -> sig != NULL){
+            // aux apunta al siguiente NODO
+            aux = aux->sig;
+        }
+        // Lo enlazamos como siguiente de 'aux'
+        aux->sig = new;
+
+    }
+}
 
 int main() {
 
     int frecuencias[6] = { 1, 1, 1, 3, 3, 9 };
     char caracteres[6] = "glza!o";
     int i;
-    Nodo nodos[6];
+    NodoArbol *p = NULL;
 
     for( i = 0; i < 6; i++ ) {
-        nodos[i].frecuencia = frecuencias[i];
-        nodos[i].caracter = caracteres[i];    
+        addTreeLef( &p, frecuencias[i], caracteres[i] );
     }
 
-    Nodo *tree = buildTree( nodos, 6 );
-    getByteCode2( tree, 'a' );
+    // Construccion del arbol
+    buildTree( p );
+
+    mostrar( p );
+
+    // NodoArbol *tree = buildTree( nodos, 6 );
+    //getByteCode( p, '0' );
+    printf("\n");
     return 0;
 }
